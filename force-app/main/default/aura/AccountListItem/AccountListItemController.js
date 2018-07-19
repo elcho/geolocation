ef42fb22867741495b30d0f1ff1dd21ff551ef3c
elcho <elchononspiegel@gmail.com>
2018-07-19 13:35:15 -0400

    wrap it up

--- force-app/main/default/aura/AccountListItem/AccountListItemController.js
+++ force-app/main/default/aura/AccountListItem/AccountListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    goToLocation : function(component, event, helper) {
+        var account = component.get("v.account");
+        var findOnMap = $A.get("e.c:AccountClicked");
+        findOnMap.setParams({"account": account});
+        findOnMap.fire();
     }
 })
--- force-app/main/default/aura/AccountMap/AccountMapController.js
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -1,20 +1,25 @@
 ({
-    jsLoaded: function(component, event, helper) {
-        var map = L.map('map', {zoomControl: false}).setView([37.784173, -122.401557], 14);
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-             {
-                 attribution: 'Tiles © Esri'
-             }).addTo(map);
-        component.set("v.map", map);
-    },
-    accountsLoaded: function(component, event, helper) {
-        // Add markers
-        var map = component.get('v.map');
-        var accounts = event.getParam('accounts');
-        for (var i=0; i<accounts.length; i++) {
-            var account = accounts[i];
-            var latLng = [account.Location__Latitude__s, account.Location__Longitude__s];
-            L.marker(latLng, {account: account}).addTo(map);
-        }
-    }
+      jsLoaded: function(component, event, helper) {
+          var map = L.map('map', {zoomControl: false}).setView([40.095287, -74.2168384], 16);
+          L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+               {
+                   attribution: 'Tiles © Esri'
+               }).addTo(map);
+          component.set("v.map", map);
+      },
+      accountsLoaded: function(component, event, helper) {
+          // Add markers
+          var map = component.get('v.map');
+          var accounts = event.getParam('accounts');
+          for (var i=0; i<accounts.length; i++) {
+              var account = accounts[i];
+              var latLng = [account.Location__Latitude__s, account.Location__Longitude__s];
+              L.marker(latLng, {account: account}).addTo(map);
+          }
+      },
+      handleFindOnMap: function(component, event, helper) {
+          var account = event.getParam("account");
+          var map = component.get("v.map").setView([account.Location__Latitude__s, account.Location__Longitude__s], 16);
+          component.set("v.map", map);
+      }
 })
